5f5252477a3df75be382bd11e5b63cd9cbfad972
Maxime Vacher-materno <maximevacher-materno@Maximes-MacBook-Pro.local>
2020-03-27 23:34:19 -0700

    add category

--- accounts/models.py
+++ accounts/models.py
@@ -16,12 +16,16 @@ class Product(models.Model):
 			) 
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    category = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True)       
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
 
 
 class Order(models.Model):
+    
 
     STATUS = (
 			('Pending', 'Pending'),
@@ -32,4 +36,4 @@ class Order(models.Model):
     #customer = 
     #product =
     date_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=200, null=True, choices = STATUS)
