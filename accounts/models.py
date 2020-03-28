937187c3551cb73d688d1181045f6e2044a80f50
Maxime Vacher-materno <maximevacher-materno@Maximes-MacBook-Pro.local>
2020-03-27 23:05:53 -0700

    add class order

--- accounts/models.py
+++ accounts/models.py
@@ -10,10 +10,26 @@ class Customer(models.Model):
         return self.name
 
 class Product(models.Model):
-     name = models.CharField(max_length=200, null=True)
-     price = models.FloatField(null=True)
-     category = models.CharField(max_length=200, null=True)
-     description = models.CharField(max_length=200, null=True)
-     date_created = models.DateTimeField(auto_now_add=True)       
+    CATEGORY = (
+			('Indoor', 'Indoor'),
+			('Out Door', 'Out Door'),
+			) 
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)       
 
 
+class Order(models.Model):
+
+    STATUS = (
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
+			)
+    
+    #customer = 
+    #product =
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200, null=True)
