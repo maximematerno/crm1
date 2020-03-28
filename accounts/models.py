4c22e04ce3bc955936fff89cc62109f09ab5ce80
Maxime Vacher-materno <maximevacher-materno@Maximes-MacBook-Pro.local>
2020-03-27 23:37:52 -0700

    add status

--- accounts/models.py
+++ accounts/models.py
@@ -37,3 +37,6 @@ class Order(models.Model):
     #product =
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices = STATUS)
+
+    def __str__(self):
+        return self.status
