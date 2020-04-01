91689e934b57b9b1e2fc952af4df445be998d174
Maxime Vacher-materno <maximevacher-materno@Maximes-MacBook-Pro.local>
2020-04-01 00:08:50 -0700

    model update

--- accounts/models.py
+++ accounts/models.py
@@ -46,4 +46,7 @@ class Order(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices = STATUS)
 
+    def __str__(self):
+        return self.status
+
     
