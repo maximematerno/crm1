100583632776c3ffba64d7d431ef9f74fa4ed0a0
Maxime Vacher-Materno <37785179+maximematerno@users.noreply.github.com>
2020-04-05 20:50:01 -0700

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,17 @@ from django.http import HttpResponse
 from .models import *
 
 def home(request):
+    order = Order.objects.all()
+    customer = Customer.objects.all()
+
+    context = {'orders' : order, 'customers': customer}
+
     return render(request, 'accounts/dashboard.html')
     
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customer(request):
-    return render(request, 'accounts/customer.html')     
+    return render(request, 'accounts/customer.html') 
