b8e52772dca31b61cf6ed003e4558e4f24c7484a
Maxime Vacher-Materno <37785179+maximematerno@users.noreply.github.com>
2020-04-13 23:35:48 -0700

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('products/', views.products, name='products'),
     path('customer/<str:pk_test>/', views.customer, name='customer'),
     path('create_order/', views.createOrder, name="create_order"),
-    path('update_order/<str:pk>', views.createOrder, name="update_order"),
+    path('update_order/<str:pk>', views.updateOrder, name="update_order"),
     
 ]
