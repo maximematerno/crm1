3172854fe19cd1aa1ab41feb5a16fe865a5d62b4
Maxime Vacher-Materno <37785179+maximematerno@users.noreply.github.com>
2020-04-22 18:17:54 -0700

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(default="lion_kingLogo.png",null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.name
