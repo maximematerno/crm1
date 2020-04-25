655a35add8da7b0588427da31272b8d39d0f10a0
Maxime Vacher-Materno <37785179+maximematerno@users.noreply.github.com>
2020-04-24 21:22:47 -0700

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -28,11 +28,6 @@ def registerPage(request):
             user = form.save()
             username = form.cleaned_data.get('username')
 
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-
-            Customer.objects.create(user=user)
-
             messages.success(request, 'Account was successfuly created for ' + username)
             return redirect('login')
 
