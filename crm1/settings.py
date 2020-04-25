e6ffb267e70b3cf45bd0d35c4a389a573f74c38a
Maxime Vacher-Materno <37785179+maximematerno@users.noreply.github.com>
2020-04-24 21:15:33 -0700

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+
+    'accounts.apps.AccountsConfig',
+
     'django_filters',
 ]
 
@@ -127,3 +129,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_ROOT= os.path.join(BASE_DIR, 'static/images')
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+
