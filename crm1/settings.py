cef392ae721007233e43d5a2c761b26f1874bd8b
Maxime Vacher-Materno <37785179+maximematerno@users.noreply.github.com>
2020-04-26 23:19:08 -0700

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -136,6 +140,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_USER='maxmat7888@gmail.com'
+EMAIL_HOST_PASSWORD='Mamama23'
 
